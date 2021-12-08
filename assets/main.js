e88f4a9239d7dda53b42d5f89925e3d4a51f4785
ismail can <ismailcankaratasss@gmail.com>
2021-12-08 07:15:40 -0800

    Desging Update3

--- assets/main.js
+++ assets/main.js
@@ -19,11 +19,11 @@ for (index = 0; index < 150; index++) {
     var mod = index % 2;
     if (  mod == 0 ) {
         $(".for").append(`
-        <tr class="working">
+        <tr>
         <td>`+index+`</td>
         <td>0</td>
         <td>3.12.2021 01:18</td>
-        <td><span class="statu">Çalışıyor</span></td>
+        <td><span class="statu Çalışıyor">Çalışıyor</span></td>
         <td>
             <div class="description">
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-box-arrow-up-left" viewBox="0 0 16 16">
@@ -35,22 +35,28 @@ for (index = 0; index < 150; index++) {
         <td>
         <div class="buttons">
             <button class="success">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-skip-start-circle-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-skip-start-circle-fill" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
             </svg>
-            Start
+            Başla
         </button>
         <button class="warning">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-pause-circle-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"" fill="currentColor" class="bi bi-pause-circle-fill" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z"/>
             </svg>
-            Pause
+            Durdur
         </button>
         <button class="danger">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-stop-circle-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-stop-circle-fill" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.5 5A1.5 1.5 0 0 0 5 6.5v3A1.5 1.5 0 0 0 6.5 11h3A1.5 1.5 0 0 0 11 9.5v-3A1.5 1.5 0 0 0 9.5 5h-3z"/>
             </svg>
-            Finish
+            Bitir
+        </button>
+        <button class="support">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-stop-circle-fill" viewBox="0 0 16 16">
+                <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.5a1 1 0 0 0-.8.4l-1.9 2.533a1 1 0 0 1-1.6 0L5.3 12.4a1 1 0 0 0-.8-.4H2a2 2 0 0 1-2-2V2zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
+            </svg>
+            Destek İste
         </button>
         </div>
         </td>
@@ -58,11 +64,11 @@ for (index = 0; index < 150; index++) {
         `);
     }else {
         $(".for").append(` 
-        <tr class="set">
+        <tr>
         <td>`+index+`</td>
         <td>0</td>
         <td>3.12.2021 01:18</td>
-        <td><span class="statu">Atandı</span></td>
+        <td><span class="statu Atandı">Atandı</span></td>
         <td>
             <div class="description">
                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-box-arrow-up-left" viewBox="0 0 16 16">
@@ -74,22 +80,28 @@ for (index = 0; index < 150; index++) {
         <td>
         <div class="buttons">
             <button class="success">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-skip-start-circle-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-skip-start-circle-fill" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
             </svg>
-            Start
+            Başla
         </button>
         <button class="warning">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-pause-circle-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50"" fill="currentColor" class="bi bi-pause-circle-fill" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z"/>
             </svg>
-            Pause
+            Durdur
         </button>
         <button class="danger">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-stop-circle-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-stop-circle-fill" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.5 5A1.5 1.5 0 0 0 5 6.5v3A1.5 1.5 0 0 0 6.5 11h3A1.5 1.5 0 0 0 11 9.5v-3A1.5 1.5 0 0 0 9.5 5h-3z"/>
             </svg>
-            Finish
+            Bitir
+        </button>
+        <button class="support">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-stop-circle-fill" viewBox="0 0 16 16">
+                <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.5a1 1 0 0 0-.8.4l-1.9 2.533a1 1 0 0 1-1.6 0L5.3 12.4a1 1 0 0 0-.8-.4H2a2 2 0 0 1-2-2V2zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
+            </svg>
+            Destek İste
         </button>
         </div>
         </td>
@@ -106,5 +118,15 @@ $(".description svg").click(function() {
 })
 $(".pop-up .close").click(function() {
     $(".pop-up-wrapper ").css("display", "none");
-
 })
+
+$(".for .Atandı").parent().parent().addClass("set");
+$(".for .Çalışıyor").parent().parent().addClass("working");
+
+
+// TODOLİST
+
+// DESTEĞİ KAPAT
+// DİĞER BUTONU - POPUP AÇILICAK BASILINCA NOT EKLENİCEK
+// SAYFA BOYDAN İNCELİCEK
+// TAMAMLANDI EKLENİCEK FARKLI RENK
